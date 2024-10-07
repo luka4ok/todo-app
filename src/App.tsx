5dca0fb29449f10341b36afffcf146abe24f23cb
luka4ok <vitalik.shostak@gmail.com>
2024-10-07 13:55:31 +0300

    name

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,63 @@
-import React from 'react';
-import './App.scss';
+import React, { useContext, useEffect, useState } from 'react';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+import { USER_ID, getTodos } from './api/todos';
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { ErrorNotification } from './components/ErrorNotification';
+import { UserWarning } from './UserWarning';
+import { Header } from './components/Header';
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
+import { Loader } from './components/Loader';
+
+import { DispatchContext, StateContext } from './store/TodoContext';
+import { useErrorMessage } from './components/useErrorMessage';
+
+import { setInputFocuseAction, setTodosAction } from './components/todoActions';
 
 export const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { todos, tempTodo } = useContext(StateContext);
+  const dispatch = useContext(DispatchContext);
+  const handleError = useErrorMessage();
+
+  useEffect(() => {
+    dispatch(setInputFocuseAction(true));
+    setIsLoading(true);
+
+    getTodos()
+      .then(todosFromServer => {
+        return dispatch(setTodosAction(todosFromServer));
+      })
+      .catch(() => {
+        handleError('Unable to load todos');
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [dispatch, handleError]);
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <Header />
+
+        {isLoading && <Loader />}
+
+        {(!!todos.length || tempTodo) && (
+          <>
+            <TodoList />
+            <Footer />
+          </>
+        )}
+      </div>
+
+      <ErrorNotification />
     </div>
   );
 };
